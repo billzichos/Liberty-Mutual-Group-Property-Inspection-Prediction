149884daa6a710da7766074237ccf9312367276b
billzichos <billzichos@hotmail.com>
2015-10-02 21:36:43 -0400

    cleaned up the gather data file

--- gather_data.R
+++ gather_data.R
@@ -11,16 +11,3 @@ source("~/GitHub/Get-Raw-Data/download.R")
 downloadKaggle("liberty-mutual-group-property-inspection-prediction","sample_submission.csv.zip")
 downloadKaggle("liberty-mutual-group-property-inspection-prediction","train.csv.zip")
 downloadKaggle("liberty-mutual-group-property-inspection-prediction","test.csv.zip")
-
-trnFile <- "train.csv"
-tstFile <- "test.csv"
-
-download.file(trnUrl, paste(wd,"\\train.csv", sep=""), quiet=TRUE)
-download.file(tstUrl, paste(wd,"\\test.csv", sep=""), quiet=TRUE)
-
-datalist <- list(
-        train=read.csv(
-                paste(wd,"\\",trnFile, sep=""), header=TRUE, as.is=TRUE), 
-        test=read.csv(
-                paste(wd,"\\",tstFile, sep=""), header=TRUE, as.is=TRUE)
-        )
